bea49fa2e6c5637917fbed8259ec567ee84316c1
jithendra0312 <kancharapujithendrakumar@gmail.com>
2026-01-08 21:58:55 +0530

    Remove Lorem Ipsum text from Footer component
    Removed placeholder text from the footer component.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,9 +7,7 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
-                    <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </p>
+            
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
                         <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
